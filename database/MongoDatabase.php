bfbf7341616702704799f6c0f834469fce85f710
goran-erhartic-devtech <goran.erhartic@devtechgroup.com>
2017-03-17 12:34:55 +0100

    Removed commented lines from the MongoDtabase class

--- database/MongoDatabase.php
+++ database/MongoDatabase.php
@@ -1,16 +1,5 @@
 <?php
 //*** MONGODB TO BE ADDED ***
-/*
-$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-
-$query = new MongoDB\Driver\Query([]);
-
-$rows = $manager->executeQuery("bla.users", $query);
-
-foreach ($rows as $row) {
-
-    echo "IME: $row->name\n";
-}*/
 
 class MongoDatabase
 {
